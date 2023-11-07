1780a39c7a233955168d20c20b2892be76b9956b
AlzubairMohammed <dev.alzubair@gmail.com>
2023-11-07 19:58:43 +0300

    Add form

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,29 @@ import SideMenu from "./components/Layout/SideMenu";
 import { Col, Row } from "react-bootstrap";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Products from "./pages/Products";
+import Products from "./pages/Products/Products";
+import AddProduct from "./pages/Products/AddProduct";
 function App() {
   return (
     <div className="App">
       <Row className="m-0 p-0">
-        <Col md="3" sm="3" xs="5">
+        <Col xl="2" lg="3" md="3" sm="3" xs="6">
           <SideMenu />
         </Col>
         <Col
           className="main-color p-0"
           style={{ height: "1000px" }}
+          xl="10"
+          lg="9"
           md="9"
           sm="9"
-          xs="7"
+          xs="6"
         >
           <NavBar />
           <Routes>
             <Route index element={<Home />} />
             <Route path="/products" element={<Products />} />
+            <Route path="/addProduct" element={<AddProduct />} />
           </Routes>
         </Col>
       </Row>
